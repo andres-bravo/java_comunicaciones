b0f232117a8cd7c3244768d62cb9e8d3f12778fa
manana <manana@Core11-41>
2018-01-30 11:08:47 +0100

    01_acceso_web V1 ejemplo URLConnection

---
+++ 01_acceso_web/src/principal/Test.java
@@ -0,0 +1,31 @@
+package principal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//Si no pongo nada utiliza puerto y recurso por defecto.
+		String dir="https://www.google.es";
+		try {
+			URL url = new URL(dir);
+			URLConnection con=url.openConnection();
+			//lectura datos
+			InputStream is=con.getInputStream();
+			//try con recursos para el cierre del fichero
+			try(BufferedReader bf = new BufferedReader(new InputStreamReader(is));){
+				bf.lines().forEach(s->System.out.println(s));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
