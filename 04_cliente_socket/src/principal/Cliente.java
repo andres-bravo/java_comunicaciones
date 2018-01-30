a44ee7ab234dbdf6c6ede4366cbf9ea795fb960b
manana <manana@Core11-41>
2018-01-30 13:56:48 +0100

    04_cliente_socket V1

---
+++ 04_cliente_socket/src/principal/Cliente.java
@@ -0,0 +1,39 @@
+package principal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		//Datos del servidor
+		String ip="10.1.1.41";
+		int puerto= 9000;
+		try {
+			//Conexión
+			Socket sc= new Socket(ip,puerto);
+			//Socket establecido ahora enviar y recibir getInputStream, getOutputStream
+			InputStream is = sc.getInputStream();
+			OutputStream os=sc.getOutputStream();
+			
+			try(PrintStream salida = new PrintStream(os);BufferedReader bf = new BufferedReader(new InputStreamReader(is));){
+				//Envío de datos
+				salida.println("profe");
+				//Leo Respuesta
+				String cad=bf.readLine();
+				System.out.println(cad);
+			}
+		}
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+				
+	}
+
+}
